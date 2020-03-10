90f6570e34aed57686c5d4ea1f0390dd426b9c21
Polun <polunwu@gmail.com>
2020-03-10 21:49:41 +0800

    add members number countup animation

--- dist/members.js
+++ dist/members.js
@@ -3,15 +3,24 @@ window.addEventListener("load", function () {
   // members page loaded
   if (document.body.classList.contains('pages-members')) {
 
-    const titles = document.querySelectorAll('.pages-members__group h3')
-    const members = document.querySelectorAll('.pages-members__member');
-    console.log(titles);
-    titles.forEach(element => {
+    document.querySelectorAll('.pages-members__group h3').forEach(element => {
       element.classList.add('animated', 'faster', 'zoomIn');
     });
-    members.forEach(element => {
+    document.querySelectorAll('.pages-members__member').forEach(element => {
       element.classList.add('animated', 'bounceInUp');
     });
+    document.querySelectorAll('.pages-members__member__count').forEach(element => {
+      countUp(element, Number(element.innerHTML), 80);
+    });
+
+    function countUp(target, endVal, intervalTime) {
+      let num = 0;
+      let counter = setInterval(() => {
+        num++;
+        target.innerHTML = num;
+        if (num == endVal) { clearInterval(counter); }
+      }, intervalTime);
+    }
 
   }
 
