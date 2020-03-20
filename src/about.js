21dca53082a6026cf8ea17ef1e6b3ffc1465c0e1
Polun <polunwu@gmail.com>
2020-03-20 22:15:31 +0800

    fix showing brand again whenever menu is opening

--- src/about.js
+++ src/about.js
@@ -1,5 +1,22 @@
 window.addEventListener("load", function () {
 
+  // 非home的頁面、滑動以隱藏 logo
+  // === mobile pages scroll to hide logo ===
+  let showBrand = true;
+  if (!document.body.classList.contains('pages-home')) {
+    const brand = document.getElementById('brand');
+    document.addEventListener('scroll', e => {
+      if (document.documentElement.scrollTop > 60) {
+        brand.style.opacity = 0;
+        showBrand = false;
+      } else {
+        brand.style.opacity = 1;
+        showBrand = true;
+      }
+    });
+  }
+  // === mobile pages scroll to hide logo ===
+
   // 每個頁面都會放 navbar toggle 功能，否則無法開啟 menu
   // === NavBar toggle to collapse control
   let showNav = false;
@@ -7,30 +24,18 @@ window.addEventListener("load", function () {
   const navToCollapse = document.getElementById('navToCollapse');
   navToggleBtn.addEventListener('click', function () {
     if (showNav) {
+      if (!showBrand) document.getElementById('brand').style.opacity = 0;
       navToCollapse.classList.remove('p-nav__links--show');
       navToggleBtn.classList.add('p-collapsed');
       showNav = !showNav;
     } else {
+      if (!showBrand) document.getElementById('brand').style.opacity = 1;
       navToCollapse.classList.add('p-nav__links--show');
       navToggleBtn.classList.remove('p-collapsed');
       showNav = !showNav;
     }
   });
   // === End of navBar toggle to collapse control
-
-  // 非home的頁面、滑動以隱藏 logo
-  // === mobile pages scroll to hide logo ===
-  if (!document.body.classList.contains('pages-home')) {
-    const brand = document.getElementById('brand');
-    document.addEventListener('scroll', e => {
-      if (document.documentElement.scrollTop > 60) {
-        brand.style.opacity = 0;
-      } else {
-        brand.style.opacity = 1;
-      }
-    });
-  }
-  // === mobile pages scroll to hide logo ===
 
   // about page 動態
   if (document.body.classList.contains('pages-about')) {
